6a613e7b461e530077f2d52c2585dcea91886bc6
María De Melo Domínguez <mdemelodominguez@gmail.com>
2022-02-23 22:30:12 +0100

    Added first routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LandingComponent } from './pages/landing/landing.component';
+
+const routes: Routes = [{ path: '', component: LandingComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
