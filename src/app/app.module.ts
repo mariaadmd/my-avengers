afa1e5fb1817ecd7e5334a15ab457ddf66e1de11
María De Melo Domínguez <mdemelodominguez@gmail.com>
2022-02-23 22:30:46 +0100

    Basic app layout created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { MaterialModule } from './material/material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LandingComponent } from './pages/landing/landing.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LandingComponent, NavbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
