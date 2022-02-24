8fb1e39f1c9d3ca74371f260297130d1afdadf8a
María De Melo Domínguez <mdemelodominguez@gmail.com>
2022-02-24 19:17:12 +0100

    Added Angular Flex Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MaterialModule } from './material/material.module';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingComponent } from './pages/landing/landing.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CharacterCardComponent } from './components/character-card/character-card.component';
 
 @NgModule({
-  declarations: [AppComponent, LandingComponent, NavbarComponent],
+  declarations: [
+    AppComponent,
+    LandingComponent,
+    NavbarComponent,
+    CharacterCardComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    FlexLayoutModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
