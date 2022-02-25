0c759458638303eec82c398db6dbd5fabaa919d3
María De Melo Domínguez <mdemelodominguez@gmail.com>
2022-02-25 01:34:51 +0100

    Added media querys mixins and various styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { MaterialModule } from './material/material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingComponent } from './pages/landing/landing.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CharacterCardComponent } from './components/character-card/character-card.component';
+import { CharacterComponent } from './pages/character/character.component';
+import { MyTeamComponent } from './pages/my-team/my-team.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
@@ -18,12 +22,16 @@ import { CharacterCardComponent } from './components/character-card/character-ca
     LandingComponent,
     NavbarComponent,
     CharacterCardComponent,
+    CharacterComponent,
+    MyTeamComponent,
+    SearchComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    FormsModule,
     FlexLayoutModule,
     HttpClientModule,
   ],
