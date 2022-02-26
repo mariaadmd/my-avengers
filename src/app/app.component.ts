27b6b578dce2247797f1ad6fba5fee890b93295f
María De Melo Domínguez <mdemelodominguez@gmail.com>
2022-02-26 03:17:23 +0100

    Added new spinner component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JarvisService } from './services/jarvis.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  loading: boolean;
+
   title = 'my-avengers';
+  constructor(private readonly jarvisService: JarvisService) {}
+
+  ngOnInit(): void {
+    console.log('init');
+
+    this.jarvisService.loading.subscribe(
+      (state: any) => (this.loading = state)
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,21 @@ import { MaterialModule } from './material/material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingComponent } from './pages/landing/landing.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { CharacterCardComponent } from './components/character-card/character-card.component';
 import { CharacterComponent } from './pages/character/character.component';
 import { MyTeamComponent } from './pages/my-team/my-team.component';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { SearchComponent } from './components/search/search.component';
-
+import { IvyCarouselModule } from 'angular-responsive-carousel';
+import { TeamFormDialogComponent } from './components/team-form-dialog/team-form-dialog.component';
+import { WelcomeDialogComponent } from './components/welcome-dialog/welcome-dialog.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
+import { LoadingInterceptor } from './interceptors/loading.interceptor';
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,17 +30,25 @@ import { SearchComponent } from './components/search/search.component';
     CharacterComponent,
     MyTeamComponent,
     SearchComponent,
+    TeamFormDialogComponent,
+    WelcomeDialogComponent,
+    SpinnerComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    InfiniteScrollModule,
+    IvyCarouselModule,
     FormsModule,
+    ReactiveFormsModule,
     FlexLayoutModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
