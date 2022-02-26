543b3e471c074a71054ea17e49184c1be9ab3ead
María De Melo Domínguez <mdemelodominguez@gmail.com>
2022-02-26 03:27:04 +0100

    Various fixes and refactor

--- src/app/pages/character/character.component.ts
+++ src/app/pages/character/character.component.ts
@@ -1,15 +1,84 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Character } from 'src/app/interfaces/character.interface';
+import { Comic } from 'src/app/interfaces/comic.interface';
+import { JarvisService } from 'src/app/services/jarvis.service';
+import { Location } from '@angular/common';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-character',
   templateUrl: './character.component.html',
-  styleUrls: ['./character.component.scss']
+  styleUrls: ['./character.component.scss'],
+  animations: [
+    trigger('simpleFadeAnimation', [
+      state('in', style({ opacity: 1 })),
+      transition(':enter', [style({ opacity: 0 }), animate(1600)]),
+    ]),
+  ],
 })
 export class CharacterComponent implements OnInit {
+  characterId: number;
+  character: Character;
 
-  constructor() { }
+  characterComics: Comic[];
 
-  ngOnInit(): void {
+  isMine: boolean;
+
+  constructor(
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly jarvisService: JarvisService,
+    private readonly location: Location
+  ) {}
+
+  async ngOnInit(): Promise<void> {
+    await this.getParams();
+
+    this.isMine = this.jarvisService.checkIsMine(this.characterId);
+
+    this.jarvisService.getCharacterById(this.characterId).subscribe((data) => {
+      console.log(data);
+      this.character = data;
+    });
+
+    this.jarvisService
+      .getCharacterComics(this.characterId)
+      .subscribe((data) => {
+        console.log(data);
+        this.characterComics = data;
+      });
   }
 
+  async getParams(): Promise<void> {
+    return new Promise<void>((resolve) => {
+      this.activatedRoute.params.subscribe((params: Params) => {
+        this.characterId = params['id'];
+        resolve();
+      });
+    });
+  }
+
+  addCharacter() {
+    this.jarvisService.addCharacter(this.character);
+    this.isMine = true;
+  }
+
+  deleteCharacter() {
+    this.jarvisService.deleteCharacter(this.character);
+    this.isMine = false;
+  }
+
+  buyComic(url: string) {
+    window.open(url, '_blank');
+  }
+
+  goBack() {
+    this.location.back();
+  }
 }
--- src/app/pages/my-team/my-team.component.ts
+++ src/app/pages/my-team/my-team.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Character } from 'src/app/interfaces/character.interface';
 import { JarvisService } from 'src/app/services/jarvis.service';
-import { StorageService } from 'src/app/services/storage.service';
+import { MatDialog } from '@angular/material/dialog';
+import { TeamFormDialogComponent } from 'src/app/components/team-form-dialog/team-form-dialog.component';
 
 @Component({
   selector: 'app-my-team',
@@ -10,18 +11,36 @@ import { StorageService } from 'src/app/services/storage.service';
 })
 export class MyTeamComponent implements OnInit {
   myTeam: Character[];
-  constructor(private readonly jarvisService: JarvisService) {}
+  myTeamInfo: any;
+  constructor(
+    private readonly jarvisService: JarvisService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.myTeam = this.jarvisService.getMyTeam();
+    this.myTeamInfo = this.jarvisService.getMyTeamInfo();
+  }
+
+  editTeamInfo(): void {
+    const dialogRef = this.dialog.open(TeamFormDialogComponent);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(result);
+
+      if (result) {
+        this.myTeamInfo = result;
+      }
+    });
   }
 
   addCharacter(character: any) {
     this.jarvisService.addCharacter(character);
   }
 
-  deleteCharacter(character: any) {
-    this.jarvisService.deleteCharacter(character);
-    this.myTeam = this.jarvisService.getMyTeam();
+  deleteCharacter(characterId: number) {
+    this.myTeam = this.myTeam.filter(
+      (teamCharacter) => teamCharacter.id !== characterId
+    );
   }
 }
