77caa4f60f2a69da9fe75070e5c08107485d8955
María De Melo Domínguez <mdemelodominguez@gmail.com>
2022-02-26 03:16:21 +0100

    Added new character routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharacterComponent } from './pages/character/character.component';
 
 import { LandingComponent } from './pages/landing/landing.component';
 import { MyTeamComponent } from './pages/my-team/my-team.component';
@@ -7,6 +8,7 @@ import { MyTeamComponent } from './pages/my-team/my-team.component';
 const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'myTeam', component: MyTeamComponent },
+  { path: 'character/:id', component: CharacterComponent },
 ];
 
 @NgModule({
